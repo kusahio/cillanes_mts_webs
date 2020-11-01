5d463b60c424e4d39a8e91198f4f4541cc37ab5e
kusahio <c.illanesdonoso@gmail.com>
2020-11-01 18:55:59 -0300

    actualizado estilos landing

--- Oviedo/landing/js/sticky.js
+++ Oviedo/landing/js/sticky.js
@@ -5,9 +5,11 @@ $(document).ready(function() {
         if (scroll > topDist.top) {
             $('.nav').addClass('fixed');
             $('.prod-container').attr('style', 'padding-top: 70px !important;' + 'margin-top:0 !important');
+            $('.cyber-blanco').attr('style', 'display:flex;');
         } else {
             $('.nav').removeClass('fixed');
             $('.prod-container').attr('style', 'padding-top: ;' + 'margin-top: ');
+            $('.cyber-blanco').attr('style', 'display:none');
         }
     });
 });
